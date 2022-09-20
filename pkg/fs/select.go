75cdddb3db985c7548c2bf249bd8095f0e091b57
yanagitani <takanori.yanagitani@gmail.com>
2022-09-21 08:42:00 +0900

    feat: file-like => name->bytes

--- pkg/fs/select.go
+++ pkg/fs/select.go
@@ -10,8 +10,43 @@ import (
 
 type FsSelect func(ctx context.Context, key FsKey) kv.Either[kv.Option[kv.Val], error]
 
+type Name2Rc func(name string) kv.Either[io.ReadCloser, error]
+
+type Name2File func(name string) kv.Either[fs.File, error]
+
+type Name2Bytes func(name string) kv.Either[[]byte, error]
+
+type Rc2Bytes func(r io.ReadCloser) kv.Either[[]byte, error]
+
 type File2Bytes func(f fs.File) kv.Either[[]byte, error]
 
-var File2bytesSimple File2Bytes = func(f fs.File) kv.Either[[]byte, error] {
-	return kv.EitherNew(io.ReadAll(f))
+type ReaderAtSize struct {
+	io.ReaderAt
+	Size int64
+}
+
+var UnlimitedRc2Bytes Rc2Bytes = func(r io.ReadCloser) kv.Either[[]byte, error] {
+	return kv.EitherNew(io.ReadAll(r))
+}
+
+var UnlimitedFile2Bytes File2Bytes = func(f fs.File) kv.Either[[]byte, error] {
+	return UnlimitedRc2Bytes(f)
+}
+
+func Name2bytesNew(r2b Rc2Bytes) func(Name2Rc) Name2Bytes {
+	return func(n2r Name2Rc) Name2Bytes {
+		return kv.ComposeEither(
+			n2r,
+			r2b,
+		)
+	}
+}
+
+func Name2bytesNewFs(f2b File2Bytes) func(Name2File) Name2Bytes {
+	return func(n2f Name2File) Name2Bytes {
+		return kv.ComposeEither(
+			n2f,
+			f2b,
+		)
+	}
 }
--- pkg/fs/zip/std/select.go
+++ pkg/fs/zip/std/select.go
@@ -5,10 +5,34 @@ import (
 	"io/fs"
 
 	kv "github.com/takanoriyanagitani/go-kvstore"
+	kf "github.com/takanoriyanagitani/go-kvstore/pkg/fs"
 )
 
-func name2fileBuilder(zr *zip.Reader) func(name string) kv.Either[fs.File, error] {
+func name2fileBuilder(zr *zip.Reader) kf.Name2File {
 	return func(name string) kv.Either[fs.File, error] {
 		return kv.EitherNew(zr.Open(name))
 	}
+}
+
+func name2BytesBuilderNew(f2b kf.File2Bytes) func(zr *zip.Reader) kf.Name2Bytes {
+	return func(zr *zip.Reader) kf.Name2Bytes {
+		var n2f kf.Name2File = name2fileBuilder(zr)
+		return kf.Name2bytesNewFs(f2b)(n2f)
+	}
+}
+
+func ras2zipReader(ras kf.ReaderAtSize) kv.Either[*zip.Reader, error] {
+	return kv.EitherNew(zip.NewReader(ras.ReaderAt, ras.Size))
+}
+
+func Name2BytesBuilderNew(ras kf.ReaderAtSize) func(kf.File2Bytes) kv.Either[kf.Name2Bytes, error] {
+	return func(f2b kf.File2Bytes) kv.Either[kf.Name2Bytes, error] {
+		var ezr kv.Either[*zip.Reader, error] = ras2zipReader(ras)
+		var z2n func(zr *zip.Reader) kf.Name2Bytes = name2BytesBuilderNew(f2b)
+		return kv.EitherMap(ezr, z2n)
+	}
+}
+
+func UnlimitedName2BytesBuilderNew(ras kf.ReaderAtSize) kv.Either[kf.Name2Bytes, error] {
+	return Name2BytesBuilderNew(ras)(kf.UnlimitedFile2Bytes)
 }

e1748042d24095d69dbc049eb29807299eecff70
yanagitani <takanori.yanagitani@gmail.com>
2022-09-21 11:00:11 +0900

    fix: more "secure"

--- pkg/fs/realfs.go
+++ pkg/fs/realfs.go
@@ -5,8 +5,22 @@ import (
 	"os"
 )
 
-type RealFs struct{}
+type RealFs struct {
+	fo fsOpen
+}
 
+func realFsNew(fo fsOpen) RealFs {
+	return RealFs{fo}
+}
+
+func RealFsNew() RealFs {
+	return realFsNew(os.Open)
+}
+
+type fsOpen func(name string) (*os.File, error)
+
+// Open tries to open named file.
+// Its users responsibility to check the name.
 func (r RealFs) Open(name string) (fs.File, error) {
-	return os.Open(name)
+	return r.fo(name)
 }
